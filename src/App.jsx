2a746b7c53493d5a2c7b54b97d061cb6f932db49
idesignerSe <hebera75@gmail.com>
2026-06-10 10:08:24 +0200

    update final working cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import ProfilePhoto from "./components/LeftColumn/ProfilePhoto.jsx";
-import Contact from "./components/LeftColumn/Contact.jsx";
-import Skills from "./components/LeftColumn/Skills.jsx";
-import Languages from "./components/LeftColumn/Languages.jsx";
+import ProfilePhoto from "./components/LeftColumn//ProfilePhoto.jsx";
+import Contact from "./components/LeftColumn//Contact.jsx";
+import Skills from "./components/LeftColumn//Skills.jsx";
+import Languages from "./components/LeftColumn//Languages.jsx";
 
 import HeaderName from "./components/RightColumn/HeaderName.jsx";
 import PersonalStatement from "./components/RightColumn/PersonalStatement.jsx";
